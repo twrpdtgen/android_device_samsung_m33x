b1c2bd505f0ae89c10879963f06ada6df46dea4d
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-05-07 09:06:33 +0200

    m33x: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: samsung Model: SM-M336BU Script version: 2.2.0

---
+++ omni_m33x.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from m33x device
+$(call inherit-product, device/samsung/m33x/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := m33x
+PRODUCT_NAME := omni_m33x
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-M336BU
+PRODUCT_MANUFACTURER := samsung
